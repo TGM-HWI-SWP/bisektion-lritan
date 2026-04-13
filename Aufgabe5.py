e623244e819b40fae87d31bc393dbed2bdda2c13
Lena Ivana Ritan <lritan@student.tgm.ac.at>
2026-04-13 16:04:17 +0200

    Erster commit yuhuu!!! Solver Docstring und initialisierung von a,b und f(a),f(b)

--- Aufgabe5.py
+++ Aufgabe5.py
@@ -1,6 +1,24 @@
+import matplotlib as mpl
 
+n = input("Gebe n an: ")
 
-def solver():
+def solver(func_str: str, a: float, b: float, tol: float = 1e-6) -> float:
+    """
+    Findet eine Nullstelle mittels Bisektion.
+    :param func_str: Die Funktion als String, z.B. "x**2 - 25"
+    :param a: Untere Intervallgrenze
+    :param b: Obere Intervallgrenze
+    :param tol: Abbruchkriterium (Genauigkeit) =1e-6 numerische Nullstelle um so nah wie möglich ans das Ergebniss zu kommen
+    """
+    
+    #Initialisierung a und b
+    a = 0
+    b = 0.2 * n
+
+    # f(x) = x^2 - n
+    fa = a**2 - n
+    fb = b**2 - n
+    
     pass
 
 
