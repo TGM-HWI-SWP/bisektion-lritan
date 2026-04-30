02274fc71ad268947b17fa93f1d6909e87ac9e1d
Lena Ivana Ritan <lritan@student.tgm.ac.at>
2026-04-30 22:05:17 +0200

    Aufgabe 9 / Seillänge berechnen

--- Aufgabe9.py
+++ Aufgabe9.py
@@ -1,8 +1,22 @@
 from Aufgabe5 import solver
 from Aufgabe6 import solver2
 from Aufgabe7 import plotter
+import math
+
+
+def berechne_seillaenge():
+    func_str = "math.cosh(50/x) - 1 - 10/x"
+
+    #solverrrrrr
+    a = solver(func_str, a=100, b=200, tol=1e-10)
+    print(f"Gefundener Krümmungsradius a = {a:.6f} m")
+
+    # Seillänge
+    l = 2 * a * math.sinh(50 / a)
+    print(f"Seillänge l = {l:.6f} m")
+
+    return a, l
 
 if __name__ == "__main__":
-    solver()
-    solver2()
-    plotter()
+    a, l = berechne_seillaenge()
+    
